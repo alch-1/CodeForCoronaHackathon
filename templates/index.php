8e9bf3db5d917ad01221ae5497116f1d07eda153
B-Lee-SH <belle.lee.2018@smu.edu.sg>
2020-05-09 15:57:15 +0800

    UI
    a lot of folders, im sorry
    
    cos im using template

---
+++ templates/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+	<html lang="zxx" class="no-js">
+	  <head>
+      <?php include 'header.html';?>
+		</head>
+		<body style="position: relative; background-image: url('util/img/background2.jpg'); width: 100%; height: 100%; background-size: cover; overflow: hidden;" >
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">
+            <img src="util/img/logo/profile.png" width="30" height="30" class="d-inline-block align-top" alt="">
+            Serenity
+            </a>
+      
+            <div class="collapse align-items-center navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Contact</a>
+                </li>
+              </ul>
+              <form class="form-inline my-2 my-lg-0">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Log-in</button>
+              </form>
+            </div>
+          </nav>		    		
+
+        <!-- start banner Area -->
+      <section class="background">
+				<div class="container">
+					<div class="row fullscreen d-flex align-items-center justify-content-start">
+						<div class="banner-content col-lg-8 col-md-12">
+							<h1 style=" color: white;">
+								Bringing You Closer					
+              </h1>
+              <h4 class="text-white text-uppercase pb-5">Lending a Listening Ear</h4>
+
+							<p class="text-white">
+								Feeling down? Afraid? Or simply just need someone to talk to? <br> Let us know how we can help.
+							</p>
+							<a href="#" class="genric-btn info circle arrow" style="font-size: 15px">Talk to Us Now <span class="lnr lnr-arrow-right"></span></a>
+						</div>												
+					</div>
+				</div>
+			</section>
+			<!-- End banner Area -->
+
+		
+
+			<!-- start footer Area -->		
+			<!-- <footer class="footer-area section-gap"> -->
+				<!-- <div class="container"> -->
+					<!-- <div class="footer-bottom d-flex justify-content-between align-items-center flex-wrap"> -->
+						<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            <!-- <p class="footer-text m-0">Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a></p> -->
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+					<!-- </div>
+				</div>
+			</footer>	 -->
+			<!-- End footer Area -->
+
+			<script src="util/js/vendor/jquery-2.2.4.min.js"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+			<script src="util/js/vendor/bootstrap.min.js"></script>			
+			<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+  			<script src="util/js/easing.min.js"></script>			
+			<script src="util/js/hoverIntent.js"></script>
+			<script src="util/js/superfish.min.js"></script>	
+			<script src="util/js/jquery.ajaxchimp.min.js"></script>
+			<script src="util/js/jquery.magnific-popup.min.js"></script>	
+			<script src="util/js/owl.carousel.min.js"></script>			
+			<script src="util/js/jquery.sticky.js"></script>
+			<script src="util/js/jquery.nice-select.min.js"></script>			
+			<script src="util/js/parallax.min.js"></script>	
+			<script src="util/js/mail-script.js"></script>	
+			<script src="util/js/main.js"></script>	
+		</body>
+	</html>
+
+
+
